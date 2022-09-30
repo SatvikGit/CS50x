1e45e4e6e0d1caba1a4cdcc00657655f106c61b3
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-09-30 09:27:18 +0000

    Fri, Sep 30, 2022, 9:27 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    // This asks the user for input
     string answer = get_string("What's your name: ");
+    // This prints hello to answer provided by the user
     printf("hello %s\n", answer);
 }
