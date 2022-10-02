a2f94f0735345205ba4b56d2caf9314a65cd8734
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-02 06:32:42 +0000

    Sun, Oct 2, 2022, 6:32 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,13 +3,22 @@
 
 int main(void)
 {
- int height = get_int("Height : ");
- if (height > 0)
- {
-    for 
- }
- else
- {
-    get_int("Height : ");
- }
+   int height;
+   do
+   {
+      height = get_int("Height : ");
+   }
+   while (height <= 0);
+   for (int i = 0; i < height; i++)
+   {
+      for (int j = 0; j <= i; j++)
+      {
+         printf("#");
+      }
+      printf("\n");
+   }
+   do
+   {
+      height = get_int("Height : ");
+   }
 }
