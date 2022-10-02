365f4d53cb93fab1c0657330b7364ef53b134d48
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-02 10:05:11 +0000

    Sun, Oct 2, 2022, 10:05 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -14,27 +14,25 @@ int main(void)
    for (int i = 0; i < height; i++)
    {
      // Prints the spaces
-     for (int j = height- 1; j > i; j--)
+     for (int j = height - 1; j > i; j--)
      {
-       printf(" ");
+      printf(" ");
      }
      // Prints the right-aligned blocks
      for (int k = 0; k <= i; k++)
      {
-        
-     }
       printf("#");
+     }
      // Prints the space between 2 sets of blocks
      for (int j = i; j >= i;  j--)
      {
-         printf("  ");
-      }
-
-      // Prints the left-aligned blocks
-      for (int k = 0; k <= i; k++)
-      {
-         printf("#");
-      }
-   printf("\n");
+      printf("  ");
+     }
+     // Prints the left-aligned blocks
+     for (int k = 0; k <= i; k++)
+     {
+      printf("#");
+     }
+     printf("\n");
    }
 }
