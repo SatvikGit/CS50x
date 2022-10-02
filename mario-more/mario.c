724a5e18cf5cd4e82fb286eca7942c1f0d78c118
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-02 07:31:51 +0000

    Sun, Oct 2, 2022, 7:31 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,18 @@ int main(void)
    while (height <= 0 || height > 8);
    for (int i = 0; i < height; i++)
    {
-      for (int j = height -1; j >1; j--)
+      for (int j = height -1; j >= i; j--)
+      {
+         printf(" ");
+      }
+      for (int k = -1; k < i; k++)
+      {
+         printf("#");
+      }
+   }
+   for (int i = 0; i < height; i++)
+   {
+      for (int j = 0; j >= i;  j--)
       {
          printf("  ");
       }
