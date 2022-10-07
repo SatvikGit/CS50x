0aa7c29edf859ad7631f9f4aeef10c1f4ffea179
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-07 07:10:05 +0000

    Fri, Oct 7, 2022, 7:10 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,5 +9,5 @@ int main(void)
     {
         ccnumber = get_long("Number: ");
     }
-    while (len(ccnumber) != 15 || len(ccnumber) != 16 || len (ccnumber));
+    while (len(ccnumber) != 15 || len(ccnumber) != 16 || len (ccnumber) != 13);
 }
