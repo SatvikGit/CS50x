a1e8ef9d681856102abe37184e84dcdec5e34107
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-09 07:26:45 +0000

    Sun, Oct 9, 2022, 7:26 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,9 +11,18 @@ int main(void)
     }
     while (ccnumber <= 0);
     int count = 0;
-    i = ccnumber;
-    while (i > 0);
+    // Counts the digits of Credit Card number
+    while (ccnumber > 0);
     {
-        
+        ccnumber = ccnumber/10;
+        count++
     }
+    // Checks if the card has valid number of digits
+    if (count != 13 && count != 15 && count!= 16);
+    {
+        printf("INVALID");
+    }
+    else
+    
+
 }
