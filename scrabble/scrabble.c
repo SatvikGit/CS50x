efd46228838c88a652a1683501255121a3fc3f4e
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-28 07:35:07 +0000

    Fri, Oct 28, 2022, 7:35 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,7 +6,8 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-char LETTERS[] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, l, n, o, p, q, r, s, t, u, v, w, x, y, z};
+char LETTERS[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
+, "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
 int compute_score(string word);
 
@@ -41,8 +42,9 @@ int compute_score(string word)
     int score = 0;
     for (int i =0; i < strlen(word); i++)
     {
-        POINTS[] = LETTERS[];
+        POINTS[i] = LETTERS[i];
         score = POINTS[i];
+        score = score + POINTS[i]
     }
     return score;
 }
