d310dc5e3cd19e25723e97acb43a4021028005c1
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-10-30 09:11:37 +0000

    Sun, Oct 30, 2022, 9:11 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string);
+
 int main(void)
 {
     string Text = get_string("Text: ");
     printf("%s\n",Text);
+}
+
+int count_letters(string)
+{
+    
 }
