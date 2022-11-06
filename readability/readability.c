35b265fea650d96a42a79f4e0daf9700b98f5846
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-06 06:57:10 +0000

    Sun, Nov 6, 2022, 6:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,6 +9,15 @@ int i;
 int main(void)
 {
     string text = get_string("Text: ");
-    i = strlen(text);
     printf("%i\n", i);
+}
+
+int count_letters(string text)
+{
+    if (islower(text) || isupper(text))
+    {
+        i = strlen(text)
+        return i
+    }
+    return i
 }
