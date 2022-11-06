7f58289fcf0356208fa7b9c7515e9531d88a25c9
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-06 07:24:53 +0000

    Sun, Nov 6, 2022, 7:24 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,22 +3,22 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text);
+char count_letters(string text);
 int i;
 
 int main(void)
 {
     string text = get_string("Text: ");
-    int number_letters = count_letters(text);
-    printf("%i\n", number_letters);
+    char number_letters = count_letters(text);
+    printf("%c\n", number_letters);
 }
 
-int count_letters(string text)
+char count_letters(string text)
 {
     for (i = 0; i < strlen(text); i++)
     {
         if (islower(text[i]) || isupper(text[i]))
-        return strlen(text);
+        return text[i];
     }
     return i;
 }
