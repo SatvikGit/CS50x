38afe6b659973f816af84d7cd777accab680912d
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-06 10:22:40 +0000

    Sun, Nov 6, 2022, 10:22 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    int i;
+    APLHA[] = [ABCDEFGHIJKLMNOPQRSTUVWXYZ]
 
+    for (int i = 0; i < 26; i++)
+    {
+        if isupper(argv[i])
+        {
+            ALPHA[i] = argv[i]
+        }
+        else if islower(argv[i])
+        {
+            ALPHA[i] = argv[i + 65]
+        }
+    }
+
+    string plain = get_string("PLAIN TEXT")
 }
