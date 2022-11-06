1cc4b864fb1e82a818c1b26188a5b3d8f9f389ec
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-06 09:02:26 +0000

    Sun, Nov 6, 2022, 9:02 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -24,7 +24,18 @@ int main(void)
 
     int index = round(0.0588*L - 0.296*S - 15.8);
 
-    
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+    else
+    printf("Grade %i\n", index);
 }
 
 float count_letters(string text)
