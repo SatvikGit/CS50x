0687f3f3efe2970d18129b12014d2c3552d09390
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-14 08:46:21 +0000

    Mon, Nov 14, 2022, 8:46 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,18 +6,4 @@ int main(int argc, string argv[])
 {
     int i;
     APLHA[] = [ABCDEFGHIJKLMNOPQRSTUVWXYZ]
-
-    for (int i = 0; i < 26; i++)
-    {
-        if isupper(argv[i])
-        {
-            ALPHA[i] = argv[i]
-        }
-        else if islower(argv[i])
-        {
-            ALPHA[i] = argv[i + 65]
-        }
-    }
-
-    string plain = get_string("PLAIN TEXT")
 }
