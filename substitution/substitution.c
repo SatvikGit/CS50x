d4b531fac9898cee68d3bf0e135e0192a3231e40
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-14 09:07:09 +0000

    Mon, Nov 14, 2022, 9:07 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -20,6 +20,9 @@ int main(int argc, string argv[])
 
     for (int i = 0; i < 26; i++)
     {
-        if (int argv[1][i] >=)
+        if (int argv[1][i] <= 65 && int argv[1][i] >= 90 || int argv[1][i] <= 97 && int argv[1][i] >= 122)
+        {
+            printf("Key must contain only letters\n")
+        }
     }
 }
