9e97b63f6d502647d874d246289c366f37bfae10
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-11-28 08:06:42 +0000

    Mon, Nov 28, 2022, 8:06 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i <= candidate_count + 1; i++)
+    for (int i = 0; i <= candidate_count; i++)
     {
         if (name == candidates[i].name)
         {
