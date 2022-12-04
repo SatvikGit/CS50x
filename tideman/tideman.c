c5ff8b613ce16346ec0e6d049d9621ef63d21d4b
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2022-12-04 14:44:21 +0000

    Sun, Dec 4, 2022, 2:44 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -182,6 +182,15 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = i+1; j < pair_count; j++)
+        {
+            locked[i][j] = true;
+            locked[j][i] = false;
+            return;
+        }
+    }
     return;
 }
 
