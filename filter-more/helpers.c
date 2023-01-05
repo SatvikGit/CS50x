ee5142d307acbf6689f508967521a88d7c302dfb
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-05 15:00:33 +0000

    Thu, Jan 5, 2023, 3:00 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -4,9 +4,9 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < width; i++)
     {
-        for (int j = 0; j < width; j++)
+        
         {
             BYTE newpixel_g[i][j];
             newpixel_g[i][j] = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
