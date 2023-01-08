47d71b3c5780d936f39de0eb7c747b42d485b1c5
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-08 09:06:20 +0000

    Sun, Jan 8, 2023, 9:06 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -29,9 +29,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
             image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
             image[i][j].rgbtRed = image[i][width - j].rgbtRed;
-            image[i][width - j].rgbtBlue = image[i][j].rgbtBlue;
-            image[i][width - j].rgbtGreen = image[i][j].rgbtGreen;
-            image[i][width - j].rgbtRed = image[i][j].rgbtRed;
         }
     }
     return;
