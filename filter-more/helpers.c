8b4c38b07939128c0f7872cb905c2b9e38f1faf2
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-13 14:48:37 +0000

    Fri, Jan 13, 2023, 2:48 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -37,6 +37,44 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = i - 1; k <= i + 1; k++)
+            {
+                for (int l = j - 1; l <= j + 1; l++)
+                {
+                    if (image[k][l] != 0)
+                    {
+                        BYTE arr_blue[] = [image[k][l]];
+                        int len_blue = sizeof(arr_blue) / sizeof(arr_blue[0]);
+                        for (int a = 0; a < len_blue; a++)
+                        {
+                            BYTE blur_blue = round(arr_blue[a] / len_blue);
+                            image[i][j].rgbtBlue = blur_blue;
+                        }
+
+                        BYTE arr_green[] = [image[k][l]];
+                        int len_green = sizeof(arr_green) / sizeof(arr_green[0]);
+                        for (int a = 0; ; a++)
+                        {
+                            BYTE blur_green = round(arr_green[a] / len_green);
+                            image[i][j].rgbtGreen = blur_green;
+                        }
+
+                        BYTE arr_red[] = [image[k][l]];
+                        int len_red = sizeof(arr_red) / sizeof(arr_red[0]);
+                        for (int a = 0; ; a++)
+                        {
+                            BYTE blur_red = round(arr_red[a] / len_red);
+                            image[i][j].rgbtRed = blur_red;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return;
 }
 
