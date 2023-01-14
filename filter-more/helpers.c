ef75052fee55eb1f7767526401ab1c1601b95272
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-14 13:46:37 +0000

    Sat, Jan 14, 2023, 1:46 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -37,40 +37,42 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            for (int k = i - 1; k <= i + 1; k++)
+            float sum_blue;
+            float sum_green;
+            float sum_red;
+            int counter;
+
+            sum_blue = sum_green = sum_red = counter = 0;
+            temp[i][j] = image[i][j];
+
+            for (int k = i - 1; k <= 1; k++)
             {
-                for (int l = j - 1; l <= j + 1; l++)
+                for (int l = j - 1; l <= 1; l++)
                 {
-                    if (image[k][l] != 0)
+                    if ((i + k) < 0 || (i + k) > height)
                     {
-                        BYTE arr_blue[] = [image[k][l]];
-                        int len_blue = sizeof(arr_blue) / sizeof(arr_blue[0]);
-                        for (int a = 0; a < len_blue; a++)
-                        {
-                            BYTE blur_blue = round(arr_blue[a] / len_blue);
-                            image[i][j].rgbtBlue = blur_blue;
-                        }
-
-                        BYTE arr_green[] = [image[k][l]];
-                        int len_green = sizeof(arr_green) / sizeof(arr_green[0]);
-                        for (int a = 0; ; a++)
-                        {
-                            BYTE blur_green = round(arr_green[a] / len_green);
-                            image[i][j].rgbtGreen = blur_green;
-                        }
-
-                        BYTE arr_red[] = [image[k][l]];
-                        int len_red = sizeof(arr_red) / sizeof(arr_red[0]);
-                        for (int a = 0; ; a++)
-                        {
-                            BYTE blur_red = round(arr_red[a] / len_red);
-                            image[i][j].rgbtRed = blur_red;
-                        }
+                        continue;
                     }
+
+                    if ((j + l) < 0 || (j + l) > width)
+                    {
+                        continue;
+                    }
+
+                    sum_blue += temp[i + k][j + l].rgbtBlue;
+                    sum_green += temp[i + k][j + l].rgbtGreen;
+                    sum_red += temp[i + k][j + l].rgbtRed;
+                    counter++;
+
+                    image[i + k][j + l].rgbtBlue = round(sum_blue / counter);
+                    image[i + k][j + l].rgbtGreen = round(sum_green / counter);
+                    image[i + k][j + l].rgbtRed = round(sum_red / counter);
                 }
             }
         }
