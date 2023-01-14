0c57408dc54da8da0f7f9d8530748aa0cad38a41
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-14 14:38:56 +0000

    Sat, Jan 14, 2023, 2:38 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -130,7 +130,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     Gy_green += temp[i + k][j + l].rgbtGreen * Gy[i][j];
                     Gy_red += temp[i + k][j + l],rgbtRed * Gy[i][j];
 
-                    image[i][j].rgbtBlue = round(sqrt((Gx_blue * Gx_blue) + ))
+                    image[i][j].rgbtBlue = round(sqrt((Gx_blue * Gx_blue) + (Gy_blue * Gy_blue)));
+                    image[i][j].rgbtGreen = round(sqrt((Gx_green * Gx_green) + (Gy_green * Gy_green)));
+                    image[i][j].rgbtRed = round(sqrt((Gx_red * Gx_red) + (Gy_red * Gy_red)));
                 }
             }
         }
