5b02ae69ce3ebc7218896d7ed50516cb66b93383
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-16 15:07:36 +0000

    Mon, Jan 16, 2023, 3:07 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -109,9 +109,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
             int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
-            for (int k = i - 1; k <= 1; k++)
+            for (int k = -1; k < 2; k++)
             {
-                for (int l = j - 1; l <= 1; l++)
+                for (int l = -1; l < 2; l++)
                 {
                     if ((i + k) < 0 || (i + k) >= height)
                     {
