90a28da2f1e6eb05a166b9b0c40e4a3f37e9c48f
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-01-29 08:35:47 +0000

    Sun, Jan 29, 2023, 2:05 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
+const B
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
 }
