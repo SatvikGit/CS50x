d5c92ed396ee9efbb0094e359a8877056f186572
SatvikGit <113091097+SatvikGit@users.noreply.github.com>
2023-03-12 07:39:30 +0000

    Sun, Mar 12, 2023, 1:09 PM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -104,7 +104,6 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    //
     for (int i = 0; i < N; i++)
     {
         node* n = table[i];
